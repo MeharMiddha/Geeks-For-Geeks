26ac1ce5d4b0e6952c2b1348767dad8ec7587023
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2024-03-04 17:01:30 +0530

    Create index.java

---
+++ Bubble Sort/index.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+    
+    
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        if(n==0 || n==1){
+            return;
+        }
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                int temp=arr[i+1];
+                arr[i+1]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        bubbleSort(arr,n-1);
+        
+    }
+}
